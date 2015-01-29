1b3661ba3e4da23fb892d323fba182dbc38b2ab6
glafarge <contact@guillaumelafarge.fr>
2015-01-29 21:23:49 +0100

    Initial commit. First attempt to solve metafizzy/flickity#14.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Flickity lazyload
+
+Enables the `lazyLoad` option for [Flickity](http://flickity.metafizzy.co)
+
+```html
+<img data-lazy="http://i.imgur.com/r8p3Xgq.jpg"/>
+```
+
+``` js
+var flkty = new Flickity( '.main-gallery', {
+  // load images ondemand or progressive
+  lazyLoad: 'ondemand' // or lazyLoad: 'progressive'
+})
+```
+
+## Install
+
+Bower: `bower install flickity-lazyload`
+
+npm: `npm install flickity-lazyload`
+
+---
+
+By [PRYSME](http://prys.me)
---
+++ flickity-lazyload.js
@@ -0,0 +1,144 @@
+/*!
+ * Flickity lazyLoad v0.1.0
+ * enables lazyLoad option for Flickity
+ * based on slick approach
+ */
+
+/*jshint browser: true, strict: true, undef: true, unused: true */
+
+( function( window, factory ) {
+	/*global define: false, module: false, require: false */
+	'use strict';
+	// universal module definition
+
+	if ( typeof define == 'function' && define.amd ) {
+		// AMD
+		define( [
+			'flickity/js/flickity',
+			'fizzy-ui-utils/utils',
+		], function( Flickity, utils ) {
+			return factory( window, Flickity, utils );
+		});
+	} 
+	else if ( typeof exports == 'object' ) {
+		// CommonJS
+		module.exports = factory(
+			window,
+			require('flickity'),
+			require('fizzy-ui-utils')
+		);
+	} 
+	else {
+		// browser global
+		window.Flickity = factory(
+			window,
+			window.Flickity,
+			window.fizzyUIUtils
+		);
+	}
+
+}( window, function factory( window, Flickity, utils ) {
+	'use strict';
+
+	Flickity.previousIndex = null; // Keep it to avoid bad behavior
+
+	Flickity.prototype.imagesLoaded = function() {
+		this.lazyLoad();
+		this.previousIndex = this.selectedIndex;
+
+		function onSelect() { 
+			if(this.selectedIndex==this.previousIndex)
+				return;
+
+			this.lazyLoad();
+			this.previousIndex = this.selectedIndex;
+		}
+		this.on('select', onSelect);
+	}
+
+	Flickity.prototype.lazyLoad = function() {
+		if ( !this.options.lazyLoad ) {
+			return;
+		}
+		var _this = this;
+
+		function onImageLoaded(e) {
+			var img = e.target;
+			eventie.unbind(img, 'load', onImageLoaded);
+
+			console.log(img.getAttribute('src') + ' loaded !');
+			img.removeAttribute('data-lazy');
+			classie.remove(img, 'flickity-loading');
+
+			// Layout
+			var cell = _this.getCell( img );
+			var cellElem = cell.element || utils.getParent( img, '.flickity-slider > *' );
+			_this.cellSizeChange( cellElem );
+		}
+
+		function loadImages(rangeStart, rangeEnd) {
+			var images = utils.filterFindElements(_this.slider.children);
+				images = images.slice(rangeStart, rangeEnd);
+
+			for ( var i=0, len = images.length; i < len; i++ ) { 
+				var img = images[i];
+				if(img.hasAttribute('data-lazy')) {
+					var url = img.getAttribute('data-lazy');
+
+					console.log('loading ' + url + '...');
+
+					eventie.bind(img, 'load', onImageLoaded);
+
+					img.src = url;
+				}
+			}
+		}
+
+
+		// ==========================================
+		// INITIALIZATION
+		// ==========================================
+
+		// Apply loading class on images that don't have src attribute but data-lazy
+		var images = utils.filterFindElements(_this.slider.children, 'img[data-lazy]');
+		for( var i=0, len = images.length; i < len; i++ ) {
+			var img = images[i];
+			if(!img.hasAttribute('src')) {
+				classie.add(img, 'flickity-loading');
+			}
+		}
+
+
+		// ==========================================
+		// LAZY LOAD ON DEMAND
+		// ==========================================
+		if(_this.options.lazyLoad == 'ondemand') {
+			// (ugly hardcoded value) :  
+			// by default 2 images loaded around the current
+			// TODO : implement as an option OR compute the value
+			var wrapNum = 2; 
+
+
+			var rangeStart = _this.selectedIndex;
+			var rangeEnd = rangeStart + wrapNum + 1;
+			loadImages(rangeStart, rangeEnd); // load next imgs
+
+			if(_this.options.wrapAround === true) {
+				rangeStart -= wrapNum;
+				if(rangeStart!=0) loadImages(rangeStart); // load prev imgs
+			}
+		}
+
+		// ==========================================
+		// PROGRESSIVE WAY
+		// ==========================================
+		else if(_this.options.lazyLoad == 'progressive') {
+			// TODO : implement it
+			// see : https://github.com/kenwheeler/slick/blob/master/slick/slick.js#L1267
+		}
+
+	};
+
+	return Flickity;
+
+}));
